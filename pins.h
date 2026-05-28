1fd182a929560eef27e912b2775f4f29cb335978
dazzaXx <48131734+dazzaXx@users.noreply.github.com>
2026-05-28 22:10:46 +0100

    Cleanup pinout for tiny

--- pins.h
+++ pins.h
@@ -18,33 +18,24 @@
 #define __PINS_H__
 
 // KER_DBG
-#define UART0_TX 0
-#define UART0_RX 1
-
-// For what future code could this be? (Hint it's for SMBUS)
-#define I2C1_SDA 2
-#define I2C1_SCL 3
+#define UART0_TX 12
+#define UART0_RX 13
 
 // SMC_DBG
-#define UART1_TX 4
-#define UART1_RX 5
+#define UART1_TX 8
+#define UART1_RX 9
 
-#define SPI_MISO 16
-#define SPI_SS_N 17
-#define SPI_CLK 18
-#define SPI_MOSI 19
-#define SMC_DBG_EN 20
-#define SMC_RST_XDK_N 21
+#define SPI_MISO 0
+#define SPI_SS_N 1
+#define SPI_CLK 2
+#define SPI_MOSI 3
+#define SMC_DBG_EN 4
+#define SMC_RST_XDK_N 5
 
-#define NUVOTON_SPI_RDY 11 // FT2V4
-#define NUVOTON_SPI_MISO 12 // FT2R7
-#define NUVOTON_SPI_SS_N 13 // FT2R6
-#define NUVOTON_SPI_CLK 14 // FT2T4
-#define NUVOTON_SPI_MOSI 15 // FT2T5
-
-#define MMC_RST_PIN 9
-#define MMC_CLK_PIN 8
-#define MMC_CMD_PIN 7
-#define MMC_DAT0_PIN 6
+#define NUVOTON_SPI_RDY 14 // FT2V4
+#define NUVOTON_SPI_MISO 15 // FT2R7
+#define NUVOTON_SPI_SS_N 26 // FT2R6
+#define NUVOTON_SPI_CLK 27 // FT2T4
+#define NUVOTON_SPI_MOSI 28 // FT2T5
 
 #endif
